8f7c63d57372bbce8b6a04bf8a15044132395648
AritaBerisha <44970964+AritaBerisha@users.noreply.github.com>
2019-04-18 19:12:55 +0200

    ndryshime te vogla

--- FIEK_TCP_Client.py
+++ FIEK_TCP_Client.py
@@ -2,16 +2,12 @@ import socket
 servername = 'localhost'
 port = 12000
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("Metodat qe mund te implementohen nga serveri jane:\n1.IPADRESA - Tregon IP-n tuaj.\n2.NUMRIIPORTIT\n3.BASHKETINGELLORE - Kthen bashketingelloret ne fjaline e dhene (BASHKETINGELLORE teksti)\n4.PRINTIMI - Printon fjalen qe ju e jepni (PRINTIMI tekti)\n5.EMRIKOMPJUTERIT - Emri i Hostuesit.\n6.KOHA - Kthen Kohen e tanishme \n7.LOJA - 7 numra te rastesishem ne intervalin[1-49].\n8.FIBONACCI\n9.KONVERTIMI - Konvertime te ndryshme: \n KilowattToHorsepower, HorsepowerToKilowatt, DegreesToRadians, RadiansToDegrees, GallonsToLiters, LitersToGallons\n10.VALIDEMAIL - tregon a eshte valide nje email\n11.PERSERITJA - tregon sa shkronja jane perseritur ne tekst\nJU LUTEMI SHKRUANI EMRAT E METODAVE ME TE MEDHA!\n" )
-s.connect((servername,port))
+socketKlient = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("Metodat qe mund te implementohen nga UDP serveri jane:\n1.IPADRESA - Tregon IP-n tuaj.\n2.NUMRIIPORTIT\n3.BASHKETINGELLORE - Kthen bashketingelloret ne fjaline e dhene (BASHKETINGELLORE teksti)\n4.PRINTIMI - Printon fjalen qe ju e jepni (PRINTIMI tekti)\n5.EMRIKOMPJUTERIT - Emri i Hostuesit.\n6.KOHA - Kthen Kohen e tanishme \n7.LOJA - 7 numra te rastesishem ne intervalin[1-49].\n8.FIBONACCI\n9.KONVERTIMI - Konvertime te ndryshme: \n KilowattToHorsepower, HorsepowerToKilowatt, DegreesToRadians, RadiansToDegrees, GallonsToLiters, LitersToGallons ( KONVERTIMI emriKonvertimit numri )\n10.VALIDEMAIL - tregon a eshte valide nje email (VALIDEMAIL email)\n11.PERSERITJA - tregon sa shkronja jane perseritur ne tekst PERSERITJA text)\nJU LUTEMI SHKRUANI EMRAT E METODAVE ME TE MEDHA!\n" )
+socketKlient.connect((servername,port))
 
 while True:
  data = input("Ju lutem shenoni nje fjali: ")
- s.sendall(str.encode(data))
- r = s.recv(1024).decode()
- #s.close()
- print('Te dhenat e pranuar nga serveri:', repr(r))
- 
-
-
+ socketKlient.sendall(str.encode(data))
+ pergjigja = socketKlient.recv(1024).decode()
+ print('Te dhenat e pranuar nga serveri:', repr(pergjigja))
